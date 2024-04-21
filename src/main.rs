f2e634132965d5738f70d55e96af095778af2434
Tnixc <tnixxc@gmail.com>
2024-04-20 21:55:09 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use tokio::fs;
+#[derive(Debug)]
+struct Replacement {
+    pub from: String,
+    pub to: String,
+}
+#[tokio::main]
+async fn main() {
+    let mut replacements: Vec<Replacement> = Vec::new();
+    let reps = fs::read_to_string("./config.toml").await.unwrap();
+    for z in reps.split("\n"){
+        if !z.starts_with("#") && z != "" {
+            let x = z.split("#").collect::<Vec<&str>>()[0];
+            let y: Vec<&str> = x.split("=").collect();
+            replacements.push(
+                Replacement {
+                    from: y[0].trim().to_string(),
+                    to: y[1].replace("\"", "").trim().to_string(),
+                }
+            );
+        }
+    }
+    for thing in replacements {
+        print!("{:?}", thing.from);
+        println!(" -> {:?}", thing.to);
+    }
+}
