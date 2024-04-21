d71e0847df6a567a8d95ed1ba14807fd709f0dd6
Tnixc <tnixxc@gmail.com>
2024-04-21 11:32:15 -0400

    done

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,64 @@
+use std::env;
 use std::fs;
+use std::path::PathBuf;
 #[derive(Debug)]
 struct Replacement {
     pub from: String,
     pub to: String,
 }
-#[tokio::main]
-async fn main() {
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <path/to/target>", args[0]);
+        return;
+    }
+    let query = &args[1];
     let mut replacements: Vec<Replacement> = Vec::new();
     let reps = fs::read_to_string("./config.toml").unwrap();
-    for z in reps.split("\n"){
+    for z in reps.split("\n") {
         if !z.starts_with("#") && z != "" {
             let x = z.split("#").collect::<Vec<&str>>()[0];
             let y: Vec<&str> = x.split("=").collect();
-            replacements.push(
-                Replacement {
-                    from: y[0].trim().to_string(),
-                    to: y[1].replace("\"", "").trim().to_string(),
-                }
-            );
+            replacements.push(Replacement {
+                from: y[0].trim().to_string(),
+                to: y[1].replace("\"", "").trim().to_string(),
+            });
         }
     }
-    // for thing in replacements {
-    //     print!("{:?}", thing.from);
-    //     println!(" -> {:?}", thing.to);
-    // }
-    // for thing in fs::read_dir("./").await.unwrap() {
+    recursive_file(&PathBuf::from(query), &replacements)
+}
 
-    let paths = std::fs::read_dir("./").unwrap();
-    for path in paths {
-        println!("{}", path.unwrap())
+fn recursive_file(path: &PathBuf, replacements: &Vec<Replacement>) {
+    let paths = fs::read_dir(path);
+    for path in paths.unwrap() {
+        let this = path.unwrap().path();
+        if this.is_dir() {
+            recursive_file(&this, replacements)
+        }
+        if this.is_file() {
+            op(&this, replacements);
+        }
+    }
+}
+
+fn op(file: &PathBuf, reqs: &Vec<Replacement>) {
+    if !file.is_file() {
+        panic!("something has gone terribly wrong");
+    }
+    let mut text = fs::read_to_string(file).unwrap_or_default();
+    if text == "" {
+        return;
+    }
+    let mut counta = 0;
+    for req in reqs {
+        if text.contains(req.from.as_str()) {
+            counta += 1;
+        }
+        text = text.replace(req.from.as_str(), req.to.as_str());
+    }
+    let _ = fs::write(file, text);
+    if counta > 0{
+        println!("{:?}, replaced {:?} terms", file, counta)
     }
 }
